cb8d0d11a69b06ba2ee99ed4ec29fd0d0cda2b21
John Knapp <knappj84@yahoo.com>
2017-07-26 15:18:45 -0400

    final submission commit

--- main.js
+++ main.js
@@ -2,54 +2,90 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // Then, write and example of using the function.
 
-function max(){
-    // Your answer here
-}
+function max (num1, num2) {
+  if (num1 > num2) {
+    return num1
+  }
+  else if (num2 > num1) {
+    return num2
+  }
 
+  else {
+    return "These Numbers are equal"
+  }
+}
+console.log(max(5, 1))
 
 // 2.
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // Then, write and example of using the function.
 
-function maxOfThree(){
-    // Your answer here
+function maxOfThree (num1, num2, num3) {
+  if ((num1 > num2) && (num1 > num3)){
+    return num1;
+  }
+
+  else if ((num2 > num1) && (num2 > num3)) {
+    return num2;
+  }
+  else{
+    return num3;
+  }
+
 }
 
-
+console.log(maxOfThree(1, 3, 5));
 // 3.
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // Then, write and example of using the function.
 
-function isVowel(char){
-    // Your answer here
+function isVowel (char) {
+  if ((char === 'a') || (char === 'e') || (char === 'i') || (char === 'o') || (char === 'u')) {
+    return true;
+  }
+    return false;
 }
 
+console.log(isVowel('b'));
 
 // 4.
 // Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
 // Then, write and example of using the function.
-
-
+function sum(num1, num2){
+  return num1+num2;
+}
+console.log(sum(1,1))
 
 // 5.
 // Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
 // Then, write and example of using the function.
+function avg(num1,num2,num3){
+  return ((num1 + num2 + num3) / 3)
+}
 
-
-
+console.log(avg(5,15,1))
 // 6.
 // Write a function called `getLength` that takes one parameter (a string) and returns the length
 // Then, write and example of using the function.
-
-
+function getLength(text){
+  return text.length;
+}
+console.log(getLength("Five"))
 
 // 7.
 // Write a function called `greaterThan` that takes two parameters
 // and returns `true` if the second parameter is greater than the first.
 // Otherwise the function should return `false`.
 // Then, write and example of using the function.
-
-
+function greaterThan(p1,p2){
+  if (p2 > p1){
+    return true
+  }
+  else {
+    return false;
+  }
+}
+console.log(greaterThan(1,5))
 
 // 8.
 // Write a function called `greet` that takes a
@@ -57,8 +93,11 @@ function isVowel(char){
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
 // Then, write and example of using the function.
+function greet (NAME){
+  return "Hello, " + NAME + "!"
+}
 
-
+console.log(greet("John"))
 
 // 9.
 // Write a function called `madlib` that takes 4 or more parameters (words).
@@ -68,3 +107,8 @@ function isVowel(char){
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 // Then, write and example of using the function.
+function madlib(noun1,verb1,noun2,adjnoun){
+  return "The " + noun1 + " wanted to " + verb1 + " " + noun2 + " after staring at the " + adjnoun + " for six hours straight";
+  }
+
+console.log(madlib("Iron Yard Student", "drink", "all the Bourbon", "dumb flexboxes"))
